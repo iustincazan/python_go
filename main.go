d0eabdb393d8912a1cdff3af642eeb33528bdaf7
Iustin Cazan <iustincazan@yahoo.com>
2023-04-14 21:05:39 +0300

    added python loader

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+// https://stackoverflow.com/questions/56586267/calling-go-from-python
+
+import (
+	/*
+	   typedef struct foo{
+	   int a;
+	   int b;
+	   int c;
+	   int d;
+	   int e;
+	   int f;
+	   } foo;
+	*/
+	"C"
+)
+
+func main() {
+}
+
+//export Foo
+func Foo(t []int) C.foo {
+	foo := C.foo{}
+	foo.a = 1 // setting some values to avoid seeing zeros
+	foo.b = 2
+	return foo
+}
